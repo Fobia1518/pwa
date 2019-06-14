cf7400920e0a1ea5e4e09a46d6d69dbca9551f34
Fobia1518 <fabian1518@hotmail.com>
2019-06-14 08:29:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// //PWA
+if ('serviceWorker' in navigator)
+{
+    navigator.serviceWorker.register('./sw.js').then(()=>{
+    console.log('Instalacion exitosa');
+ }, (err)=> {
+    console.log('Instalacion fallida: ' + err);
+ });
+}  
